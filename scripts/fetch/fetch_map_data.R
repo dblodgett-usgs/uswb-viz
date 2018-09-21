6754b9e28e42d4c06592bcd0bd29019c6a48e0b7
David Blodgett <dblodgett@usgs.gov>
2018-09-21 13:31:24 -0500

    Add to footer, update elfie data, and modify animate.js

--- scripts/fetch/fetch_map_data.R
+++ scripts/fetch/fetch_map_data.R
@@ -32,7 +32,7 @@ fetch.fetch_map_data <- function(viz = as.viz("fetch_map_data")) {
   }
   
   for(i in 1:length(map_data)) {
-    map_data[[i]] <- sf::st_sf(geometry = sf::st_sfc(map_data[[i]]), 
+    map_data[[i]] <- sf::st_sf(geometry = sf::st_as_sfc(lapply(map_data[[i]], function(x) x[[1]][[1]])), 
                                data.frame(huc12 = HU_ids, 
                                           row.names = HU_ids, 
                                           stringsAsFactors = F))
@@ -52,14 +52,15 @@ parse_elfie_json <- function(url) {
   name <- jl$`@type`
   out <- list()
   if(!is.null(jl$geo) && !is.null(jl$geo$`@type`)) {
-    if(jl$geo$`@type` == "schema:GeoCoordinates") {
-      sfg <- sf::st_point(c(jl$geo$longitude, jl$geo$latitude))
+    if(any(jl$geo$`@type` == "schema:GeoCoordinates")) {
+      sfg <- sf::st_point(c(jl$geo$`schema:longitude`[1], jl$geo$`schema:latitude`[1]))
       jl$geo <- sfg
       out[[name]] <- jl$geo
     }
-    if(!is.null(jl$geometry)) {
-      names(jl$geometry)[which(names(jl$geometry) == "@type")] <- "type"
-      jl$geometry <- sf::read_sf(jsonlite::toJSON(jl$geometry, auto_unbox = T))$geometry[[1]]
+    if(!is.null(jl$`gsp:hasGeometry`)) {
+      names(jl$`gsp:hasGeometry`)[which(names(jl$`gsp:hasGeometry`) == "@type")] <- "type"
+      jl$geometry <- sf::st_sf(geom = sf::st_as_sfc(jl$`gsp:hasGeometry`$`gsp:asWKT`), 
+                               crs = "+init=epsg:4326")
       out[[name]] <- jl$geometry
     }
   }
